25eab83ccb447fa73f65aaba2b3cfaddde5671c4
Francesco Leuzzi <90636442+frame88@users.noreply.github.com>
2022-02-28 16:40:59 +0100

    moltiplicazione di una stringa data in input

---
+++ 1.8.py
@@ -0,0 +1,7 @@
+word_1 = input('inserisci una parola: ')      #esempio: casa
+num = int(input('inserisci un\' intero: '))     #esempio: 3
+
+c = word_1 * num      #esempio: casacasacasa
+
+
+print('La variabile word_1 moltiplicata per', num, 'produce:', c) 
